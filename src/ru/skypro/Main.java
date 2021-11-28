7c75c0586a5d45758b71c932f58f68d97c65b578
hope2183@mail.ru <hope2183@mail.ru>
2021-11-28 21:37:04 +1100

    Branch Commit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,7 +2,39 @@ package ru.skypro;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static <string> void main(String[] args) {
+	// TASK 1
+        int start = 1;
+        while (start<=10){
+            System.out.print(start+" ");
+            start++;
+        }
+        System.out.println();
+
+        for (start=10; start>0; start--) {
+            System.out.print(start+" ");
+        }
+        System.out.println();
+
+    //TASK 2
+
+        for(int firstFriday = 6; firstFriday<=31;firstFriday+=7) {
+            System.out.println("Пятница, " + firstFriday + " число! Сдать отчет!!!");
+        }
+
+    //TASK 3
+
+        int currentYear = 2021;
+        int startYear = currentYear-200;
+        int endYear = currentYear+100;
+
+        for(currentYear=startYear; currentYear<=endYear; currentYear++){
+
+            if (currentYear%79==0){
+                System.out.println(currentYear);
+            }
+
+        }
+
     }
 }
